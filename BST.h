255fd158a4f1b869a1d592e0bb09709c99b0dc09
GlenMerlin <Glenmerlin@protonmail.com>
2022-03-15 16:10:17 -0600

    small bug fixes

--- BST.h
+++ BST.h
@@ -6,21 +6,13 @@ using namespace std;
 class BST : public BSTInterface {
     public:
         BST();
-
         virtual ~BST(void);
-
         Node * getRootNode() const;
-        
         bool add(int data);
-
         bool updateData(Node*& currentNode, int data);
-        
         bool remove(int data);
-
         bool deleteData(Node*& currentNode, int data);
-
         void clearHelper(Node*& currentNode);
-        
         void clear();
     
     private:
